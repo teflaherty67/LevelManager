96dd70ea959f93a699f64f4eb24e93aa8c7cd46b
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2025-08-20 19:45:29 -0500

    Add project files.

---
+++ LevelManager/Common/Utils.cs
@@ -0,0 +1,45 @@
+﻿
+namespace LevelManager.Common
+{
+    internal static class Utils
+    {
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel curPanel;
+
+            if (GetRibbonPanelByName(app, tabName, panelName) == null)
+                curPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            else
+                curPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            return curPanel;
+        }
+
+        internal static List<View> GetAllSectionViews(Document curDoc)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static List<Level> GetFilteredAndSortedLevels(Document curDoc)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tmpPanel.Name == panelName)
+                    return tmpPanel;
+            }
+
+            return null;
+        }
+
+        internal static void TaskDialogInformation(string v1, string v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LevelManager/cmdLevelManager.cs
@@ -0,0 +1,71 @@
+﻿using LevelManager.Common;
+
+namespace LevelManager
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdLevelManager : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document curDoc = uidoc.Document;
+
+            #region Get Data
+
+            // get filtered & sorted list of levels in the document
+            List<Level> filteredLevels = Utils.GetFilteredAndSortedLevels(curDoc);
+
+            // get all the ViewSection views
+            List<View> listViews = Utils.GetAllSectionViews(curDoc);
+
+            // get the first view whose Title on Sheet is "Front Elevation"
+            View elevFront = listViews
+                .FirstOrDefault(v => v.get_Parameter(BuiltInParameter.VIEW_DESCRIPTION)?.AsString() == "Front Elevation");
+
+            // set that view as the active view
+            if (elevFront != null)
+            {
+                uidoc.ActiveView = elevFront;
+            }
+            else
+            {
+                Utils.TaskDialogInformation("Information", "Level Manager", "Front Elevation view not found. Proceeding with level management in current view.");
+            }
+
+            // create a counter for summary report
+            int countAdjusted = 0;
+
+
+
+
+
+
+            #endregion
+
+            // get data for form
+
+            //
+
+            return Result.Succeeded;
+        }
+
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+
+            Common.ButtonDataClass myButtonData = new Common.ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData.Data;
+        }
+    }
+}
